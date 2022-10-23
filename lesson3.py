237db62698985efd9241cf289b0e6b42aea085cb
9Produman <114502348+9Produman@users.noreply.github.com>
2022-10-23 17:14:42 +0300

    Update lesson3.py

--- lesson3.py
+++ lesson3.py
@@ -1,14 +1,11 @@
-input_string = input()
-values = input_string.split()
-count = len(values)
 if count == 1:
     r = int(input_string)
     if r <= 0 :
         print('Такий круг не може існувати')
     else:
-        s = 3,14 * r ** 2
         p = 2 * 3,14 * r
-        print(f'Circle: {r}; {s}; {p}')
+        s = 3, 14 * r ** 2
+        print(f'Circle: r = {r}; perimeter = {p}; square = {s}')
 
 elif count == 2:
     a, b = values
@@ -17,9 +14,9 @@ elif count == 2:
     if a1 <= 0 or b1 <= 0:
         print('Такий прямокутник не може існувати')
     else:
-        s = a1 * b1
         p = (a1 + b1) * 2
-        print(f'Rectangle: {a1} {b1}; {s}; {p}')
+        s = a1 * b1
+        print(f'Rectangle: a = {a1}, b = {b1}}; perimeter = {p}; square = {s}')
 elif count == 3:
     a, b, c = values
     a1 = int(a)
@@ -28,6 +25,6 @@ elif count == 3:
     if a1 <= 0 or b1 <= 0 or c1 <= 0:
         print('Такий трикутник не може існувати')
     else:
-        s = a1 * b1
         p = a1 + b1 + c1
-        print(f'Triangle: {a1} {b1} {c1}; {s}; {p}')
+        s = a1 * b1 * 0,5
+        print(f'Triangle: a = {a1}, b = {b1}, c = {c1}; perimeter = {p}; square = {s}')
